b723aed5921836cc5b2884535cc382d9013ac826
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-25 01:42:49 -0400

    bootimage dependencies

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -3,15 +3,25 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // name '_start' by default
-    loop {}
-}
-
 // need to have a panic handler in a no std environment
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    
+    // this function is the entry point, since the linker looks for a function
+    // name '_start' by default
     loop {}
 }
